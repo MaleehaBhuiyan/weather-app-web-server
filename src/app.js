151982f65a881416b29941bcb6efb7a54bd58513
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-03-29 08:45:27 -0400

    Handlebars implemented

--- src/app.js
+++ src/app.js
@@ -6,9 +6,32 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 const aboutDirectoryPath = path.join(__dirname, '../public/about')
 const helpDirectoryPath = path.join(__dirname, '../public/help')
 
+
+app.set('view engine','hbs')
 app.use(express.static(publicDirectoryPath))
-app.use(express.static(aboutDirectoryPath))
-app.use(express.static(helpDirectoryPath))
+// app.use(express.static(aboutDirectoryPath))
+// app.use(express.static(helpDirectoryPath))
+
+app.get('', (req, res) => {
+    res.render('index', {
+        title: 'Weather App',
+        name: 'Maleeha Bhuiyan'
+    })
+})
+
+app.get('/about', (req, res) => {
+    res.render('about', {
+        title: 'About Me',
+        name: "Maleeha Bhuiyan"
+    })
+})
+
+app.get('/help', (req, res) => {
+    res.render('help', {
+        title: 'Help Page',
+        message: "Help Message"
+    })
+})
 
 app.get('', (req, res) => {
     res.send('<h1>Weather</h1>')
