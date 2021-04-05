db9f675e2c0ac31fc41508bc774c3abae3804d95
MaleehaBhuiyan <maleeha.bhuiyan@baruchmail.cuny.edu>
2021-04-04 20:47:05 -0400

    Added fetch call

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,19 @@
 console.log('Client Side javascript file is loaded! ')
+
+fetch('http://localhost:3000/weather?address=!')
+.then((resp) => {
+    resp.json().then((data) => {
+        if (data.error){
+            return console.log("Invalid Data")
+        }
+        console.log("Location: ", data.location)
+        console.log("Forecast: ", data.forecast.success)
+    })
+})
+
+// fetch('http://puzzle.mead.io/puzzle')
+// .then((resp) => {
+//     resp.json().then((data) => {
+//         console.log(data)
+//     })
+// })
